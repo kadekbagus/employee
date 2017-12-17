16e544917f184fb3929bdc4bf94226e729f93dfe
Kadek Bagus <kadekdarma90@gmail.com>
2017-12-17 10:57:04 -0500

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+	apiKey: "AIzaSyBZIDvRfmTQavvuKWf4blrwWu7WRqKCL8A",
+    authDomain: "vue-learn-4bb6f.firebaseapp.com",
+    databaseURL: "https://vue-learn-4bb6f.firebaseio.com",
+    projectId: "vue-learn-4bb6f",
+    storageBucket: "vue-learn-4bb6f.appspot.com",
+    messagingSenderId: "945717818677"
+}
